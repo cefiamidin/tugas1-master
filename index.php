b58f25e80b1f12bca1ce24dd904e671b7beda376
cefiamidin <cefiamidin@mahasiswa.unikom.ac.id>
2019-10-11 22:31:59 +0700

    penambahan link tugas individu

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
         <td><<a href="https://tugas1-101162xx-11.herokuapp.com/" h4 class="text-white"> Cefi Amidin M | </a></h4></td>
         <td><h4 class="text-white"> M Naufal fadhlur R | </h4></td>
         <td><h4 class="text-white"> Yogi Kusuma Santika | </h4></td>
-        <td><h4 class="text-white"> M Reza Febriana | </h4></td>
+        <td><a href="https://tugas1-101162xx-28.herokuapp.com/"h4 class="text-white"> M Reza Febriana | </h4></td>
         <td><h4 class="text-white"> Rangga R </h4></td>
               </div>
 			  </center>
